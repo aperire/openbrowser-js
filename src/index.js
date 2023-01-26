c89ba86997f8597624b6d99d489ee2d1d4418dca
Joohwan Lee <aperire0402@gmail.com>
2023-01-26 18:38:11 -0500

    full debug/test case

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 const { OpenBrowser } = require('./OpenBrowser');
+const fs = require('fs');
+let openBrowser = new OpenBrowser("http://localhost:3000");
 
-let openBrowser = new OpenBrowser("");
+const processAndPostData = async() => {
+    // 1. Fetch RPC Array
+    const rpcArray = await openBrowser.getAvailableRpcs();
 
-let {b64EncryptedFile, publicKey} = openBrowser.processFile(
-    "./img/img.jpeg"
+    // 2. Process File
+    let bitmap = fs.readFileSync("./img/img.jpeg");
+    let { b64EncryptedFile, publicKey} = openBrowser.processFileWithBitmap(bitmap);
+
+    // 3. Distribute Data to RPC/RPCs
+    const result = await openBrowser.distributeDataToRpc(
+        publicKey, b64EncryptedFile, rpcArray, "jpeg", "img.jpeg" 
+    );
+}
+
+const retrieveAndSaveData = async(publicKey, retrieveDirectory, rpcEndpoint) => {
+    let status = openBrowser.retrieveAndStoreDataFromRpc(
+        publicKey, retrieveDirectory, rpcEndpoint
+    )
+}
+
+//processAndPostData();
+retrieveAndSaveData(
+    "ca38e05bba85c1077a3ee4fb620104486a84fb7ee8fd99a0c2518dc74093fe05",
+    "retrieve_img",
+    "http://localhost:8080"
 );
-console.log(b64EncryptedFile.length);
--- src/openbrowser.js
+++ src/openbrowser.js
@@ -10,45 +10,47 @@ class OpenBrowser {
 
     processFileWithBitmap = (bitmap) => {
         // base64 encode
-        const b64EncryptedImage = Buffer.from(bitmap).toString('base64');
-        const publicKey = shajs("sha256").update(b64EncryptedImage).digest("hex");
+        const b64EncryptedFile = Buffer.from(bitmap).toString('base64');
+        const publicKey = shajs("sha256").update(b64EncryptedFile).digest("hex");
 
         return { b64EncryptedFile, publicKey };
     }
     processFileWithPath = (filePath) => {
         // base64 encode
         const bitmap = fs.readFileSync(filePath);
-        const b64EncryptedImage = Buffer.from(bitmap).toString('base64');
-        const publicKey = shajs("sha256").update(b64EncryptedImage).digest("hex");
+        const b64EncryptedFile = Buffer.from(bitmap).toString('base64');
+        const publicKey = shajs("sha256").update(b64EncryptedFile).digest("hex");
 
         return { b64EncryptedFile, publicKey };
     }
 
     distributeDataToRpc = async(publicKey, b64EncryptedFile, rpcArray, fileType, fileName) => {
-        assert(this.pingRpc(rpcArray).length == 0, "One of the storage Offline");
-        const postData = {
-            publicKey: [b64EncryptedFile, fileType, fileName]
-        };
-        for (i=0; i<rpcArray.length; i++) {
-            let res = await axios.post(`${i}/data`, postData);
+        assert(this.pingRpc(rpcArray).length != 0, "One of the storage Offline");
+        const postData = {}
+        postData[publicKey] = [b64EncryptedFile, fileType, fileName]
+        
+        for (let i=0; i<rpcArray.length; i++) {
+            let res = await axios.post(`${rpcArray[i]}/data`, postData);
         }
         return true;
     }
 
-    retrieveDataFromRpc = async(publicKey, retrieveDirectory, rpcEndpoint) => {
-        const res = await axios.get(`${rpcEndpoint}/retrieve`);
-        require(res.status == 200, `Endpoint - ${rpcEndpoint} unresponsive`);
+    retrieveDataFromRpc = async(publicKey, rpcEndpoint) => {
+        const res = await axios.post(`${rpcEndpoint}/retrieve`, {
+            "pubkey": publicKey
+        });
 
-        const data = res.data[publicKey];
+        const data = res.data.data;
         const bitmap = Buffer.from(data[0], 'base64');
         return bitmap;
     }
 
     retrieveAndStoreDataFromRpc = async(publicKey, retrieveDirectory, rpcEndpoint) => {
-        const res = await axios.get(`${rpcEndpoint}/retrieve`);
-        require(res.status == 200, `Endpoint - ${rpcEndpoint} unresponsive`);
+        const res = await axios.post(`${rpcEndpoint}/retrieve`, {
+            "pubkey": publicKey
+        });
 
-        const data = res.data[publicKey];
+        const data = res.data.data;
         const bitmap = Buffer.from(data[0], 'base64');
         fs.writeFileSync(`${retrieveDirectory}/${data[2]}`, bitmap);
         return true;
@@ -63,8 +65,8 @@ class OpenBrowser {
     pingRpc = async(rpcArray) => {
         let offlineRpcArray = [];
 
-        for (i=0; i<rpcArray.length; i++) {
-            let res = await axios.get(`${i}/ping`, validateStatus=true);
+        for (let i=0; i<rpcArray.length; i++) {
+            let res = await axios.get(`${rpcArray[i]}/ping`);
             if (res.status != 200 || res.data.status == false) {
                 offlineRpcArray.push(i);
             }
